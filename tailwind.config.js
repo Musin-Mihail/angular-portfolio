06274dbfeff6cb57b7897c8122a1865b4d0db159
Мусин Михаил <musin.mihail@gmail.com>
2025-09-06 15:30:51 +0300

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Эта строка говорит Tailwind, где искать классы
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
