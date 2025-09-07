718e65e85e6ea14900a3d11e600b3224c8b2510e
Мусин Михаил <musin.mihail@gmail.com>
2025-09-07 20:37:50 +0300

    Мобильная навигация

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 const NAV_LINKS = [
   { path: '/', label: 'Обо мне', options: { exact: true } },
   { path: '/projects', label: 'Проекты', options: { exact: false } },
   { path: '/lab', label: 'Лаборатория', options: { exact: false } },
 ];
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,4 +15,8 @@ const NAV_LINKS = [
 })
 export class AppComponent {
   public readonly navLinks = NAV_LINKS;
+  public isMobileMenuOpen = signal(false);
+  public closeMobileMenu(): void {
+    this.isMobileMenuOpen.set(false);
+  }
 }
