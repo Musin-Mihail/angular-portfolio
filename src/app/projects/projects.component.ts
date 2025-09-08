38b0d5edbe385e6554c0997a8de6a77550ef4614
Мусин Михаил <musin.mihail@gmail.com>
2025-09-08 16:30:46 +0300

    fix Анимация и переходы

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -23,27 +23,13 @@ const GITHUB_USERNAME = 'Musin-Mihail';
   styleUrls: ['./projects.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
-    trigger('listAnimation', [
-      transition('* => *', [
-        query(
-          ':leave',
-          [
-            stagger('50ms', [
-              animate('300ms ease-in', style({ opacity: 0, transform: 'scale(0.9)' })),
-            ]),
-          ],
-          { optional: true }
-        ),
-        query(
-          ':enter',
-          [
-            style({ opacity: 0, transform: 'scale(0.9)' }),
-            stagger('50ms', [
-              animate('300ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
-            ]),
-          ],
-          { optional: true }
-        ),
+    trigger('itemAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.9)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ opacity: 0, transform: 'scale(0.9)' })),
       ]),
     ]),
   ],
